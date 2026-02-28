ebaa7f8fac86cb641be3eb1116b63106a051ea5a
Ch8s <chetanasanthi.m@gmail.com>
2026-02-28 21:56:03 +0530

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	todo.txt

--- main.py
+++ main.py
@@ -4,10 +4,12 @@ while True:
 
     match user_action:
         case 'add':
-            with open('todo.txt','r') as file:
-                file.readlines(Todos)
+            todo = input('Enter a todo: ') + '\n'
 
-            Todos.append(Todos)
+            with open('todo.txt','r') as file:
+                todos = file.readlines()
+
+            Todos.append(todo)
 
             with open('todo.txt','w') as file:
                 file.writelines(Todos)
@@ -15,13 +17,22 @@ while True:
         case 'edit':
             number = int(input('number of the task to be edited: '))
             number = number - 1  # ensures that the numbering starts from 1, not 0
+
+            with open('todo.txt','r') as file:
+                todos = file.readlines()
+                print('here are the existing todos: ', Todos)
+
             new_todo = input('Enter new todo: ')
             Todos[number] = new_todo
+            print('edited list of todos: ', Todos)
 
         case 'show' | 'display':  # bitwise op. can use either show or display
             file = open('todo.txt','r')
             Todos = file.readlines()
             file.close()
+
+            with open('todo.txt','r') as file:
+                todos = file.readlines()
 
             new_todos = [item.strip('\n')for item in Todos] #List Comprehension
 
