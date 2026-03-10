450a3bdc448a206ee54c0b8c68b5d7b3eee52d7e
Ch8s <chetanasanthi.m@gmail.com>
2026-03-10 21:21:05 +0530

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	todo.txt

--- main.py
+++ main.py
@@ -3,78 +3,68 @@ def get_todos():
         todos = file.readlines()
     return todos
 
-    while True:
+
+while True:
     user_action = input("Type add, edit, show, complete or exit: ")
     user_action = user_action.strip()
 
     if user_action.startswith('add'):
         todo = user_action[4:]
-
-        get_todos()
-
+        todos = get_todos()
         todos.append(todo + '\n')
 
-        with open('todo.txt','w') as file:
+        with open('todos.txt', 'w') as file:
             file.writelines(todos)
 
     elif user_action.startswith('edit'):
         try:
             number = int(user_action[5:])
-            print(number)
-
             number = number - 1
 
             todos = get_todos()
-
             new_todo = input('Enter new todo: ')
             todos[number] = new_todo + '\n'
 
-            with open('todo.txt','w') as file:
+            with open('todos.txt', 'w') as file:
                 file.writelines(todos)
         except ValueError:
             print('Invalid Command')
             continue
+        except IndexError:
+            print("No such todo item.")
+            continue
 
     elif user_action.startswith('show'):
-        with open('todo.txt','r') as file:
-            todos = file.readlines()
-
+        todos = get_todos()
         new_todos = [item.strip('\n') for item in todos]
 
         print("Total number of tasks:", len(todos))
         for index, item in enumerate(new_todos):
-            row = f"{index+1}. {item}"
+            row = f"{index + 1}. {item}"
             print(row)
 
-
     elif user_action.startswith('complete'):
-
         try:
-
             number = int(user_action[9:])
-
             todos = get_todos()
-
             todo_to_remove = todos.pop(number - 1).strip('\n')
 
-            with open('todo.txt', 'w') as file:
-
+            with open('todos.txt', 'w') as file:
                 file.writelines(todos)
 
             message = f"Todo '{todo_to_remove}' was removed from the list."
-
             print(message)
 
-
-        except IndexError:
-
-            print("No such todo item.")
-
+        except ValueError:
+            print("Invalid Command")
             continue
-
+        except IndexError:
+            print("No such todo item.")
+            continue
 
     elif user_action.startswith('exit'):
         break
+
     else:
         print('Invalid Command')
 
