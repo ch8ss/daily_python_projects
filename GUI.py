b6c5918ce90bfdf0e1f3f1dbd1a6b502646e51cc
Ch8s <chetanasanthi.m@gmail.com>
2026-03-17 17:31:59 +0530

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	todo.txt

--- GUI.py
+++ GUI.py
@@ -1,11 +1,15 @@
 import functions
-import PySimpleGUI as sg
+import FreeSimpleGUI as sg
 
 label = sg.Text("Type in a to-do")
-input_box = sg.InputText(tooltip = "Enter todo")
+input_box = sg.InputText(tooltip="Enter todo", key="todo")
 add_button = sg.Button("Add")
 
-window = sg.Window('My Todo App', layout = [[label], [input_box, add_button]])
+window = sg.Window("My To-Do App",
+                   layout=[[label], [input_box, add_button]],
+                   font=("Helvetica", 20))
 
-window.read()
-print("Hello")
+event = window.read()
+print(event)
+
+window.close()
